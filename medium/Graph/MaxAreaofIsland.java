578cbabe36af8577b997ccd0f4766fa9dbaf5d8f
yidanwang214 <yidan.eden.wang@gmail.com>
2024-05-10 12:32:59 +0930

    10 May 2024

--- medium/Graph/MaxAreaofIsland.java
+++ medium/Graph/MaxAreaofIsland.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaxAreaofIsland {
 
     /*
@@ -40,4 +43,58 @@ public class MaxAreaofIsland {
         area += dfs(i - 1, j, grid) + dfs(i + 1, j, grid) + dfs(i, j - 1, grid) + dfs(i, j + 1, grid);
         return area;
     }
+
+    /*
+     * bfs
+     */
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        this.visited = new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 && visited[i][j] == 0) {
+                    Queue<int[]> q = new LinkedList<>();
+                    int[] coordinate = { i, j };
+                    q.add(coordinate);
+                    int area = 0;
+                    while (!q.isEmpty()) {
+                        area++;
+                        int[] tmp = q.poll();
+                        int row = tmp[0];
+                        int col = tmp[1];
+                        visited[row][col] = 1;
+                        System.out.println("area: " + area + ", i: " + row + ", j: " + col);
+                        // up
+                        if (row - 1 >= 0 && grid[row - 1][col] == 1 && visited[row - 1][col] == 0) {
+                            int[] arr = { row - 1, col };
+                            q.add(arr);
+                            visited[row - 1][col] = 1;
+                        }
+                        // down
+                        if (row + 1 < grid.length && grid[row + 1][col] == 1 && visited[row + 1][col] == 0) {
+                            int[] arr = { row + 1, col };
+                            q.add(arr);
+                            visited[row + 1][col] = 1;
+                        }
+                        // left
+                        if (col - 1 >= 0 && grid[row][col - 1] == 1 && visited[row][col - 1] == 0) {
+                            int[] arr = { row, col - 1 };
+                            q.add(arr);
+                            visited[row][col - 1] = 1;
+                        }
+                        // right
+                        if (col + 1 < grid[0].length && grid[row][col + 1] == 1 && visited[row][col + 1] == 0) {
+                            int[] arr = { row, col + 1 };
+                            q.add(arr);
+                            visited[row][col + 1] = 1;
+                        }
+                    }
+                    max = Math.max(max, area);
+                    System.out.println(max);
+                }
+            }
+        }
+        return max;
+    }
+
 }
